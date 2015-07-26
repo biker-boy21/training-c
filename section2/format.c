50f45efe223ce0b4a4b47c3bdffd6b9c2db2eadc
d.j.smith <dean_rules_21@hotmail.co.uk>
2015-07-26 15:18:28 +0100

    new file:   dweight.c 	new file:   format.c 	new file:   helloworld.c 	new file:   volumesphere.c

---
+++ section2/format.c
@@ -0,0 +1,15 @@
+/*this program identifies the directives and statements within a typical C program structure */
+
+#include<stdio.h>
+// directive: identifies the precompiled bits
+
+int main(void)
+{
+  /* this starts the main function with no inputs(void) and expects an integer       value at the end
+   */
+  printf("these are statements\n");
+  return 0;
+}
+
+  
+
